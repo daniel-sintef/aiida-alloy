8754b91c08587e7b09ba40921f2b05861d621321
Daniel Marchand <daniel.marchand@gmail.com>
2019-03-06 17:02:30 +0100

    added parent-tracking capacity to load_runner

--- aiida_load_runner_dataset_as_aiida_group.py
+++ aiida_load_runner_dataset_as_aiida_group.py
@@ -2,18 +2,31 @@
 import aiida
 aiida.load_dbenv()
 from aiida.orm.group import Group
+from aiida.orm.utils import load_node
 from aiida.orm.data.structure import StructureData
 import ase
 import ase.io
 import click
+
+def add_parentstructure_extras(structurenode, parent_uuid):
+    # NOTE: consider adding a check if parent_extras is already assigned
+    structure_extras = structurenode.get_extras()
+    parent_extras = load_node(parent_uuid).get_extras()
+    for key, value in parent_extras.items():
+        if key not in structure_extras:
+            structurenode.set_extra(key, value)
+    structurenode.set_extra('parent_extras', True)
+    return
 
 
 @click.command()
 @click.option('-d', '--dataset_path', required=True)
 @click.option('-gn', '--group_name', required=True)
 @click.option('-gd', '--group_description', default="")
-@click.option('-pc', '--parse_comments', default=False)
-def launch(dataset_path, group_name, group_description, parse_comments):
+@click.option('-pcp', '--parse_comments_path', is_flag=True)
+@click.option('-pcs', '--parse_comments_structure', is_flag=True)
+def launch(dataset_path, group_name, group_description, 
+           parse_comments_path, parse_comments_structure):
     print "loading dataset: {} to group: {}".format(dataset_path, group_name)
 
     # Setup/Retrieve the Group
@@ -31,13 +44,23 @@ def launch(dataset_path, group_name, group_description, parse_comments):
         aiida_structure_stored = aiida_structure.store()
 
         # add in the dataset_path line if possible
-        if parse_comments:
+        if parse_comments_path:
             try:
                 structure_path = ase_structure.comment.strip().split()[-1][3:]
                 aiida_structure_stored.set_extra("structure_path", structure_path)
             except AttributeError:
                 print "could not set structure_path on {}".format(ase_structure)
                 pass
+        # Add in details of parent structure uuid
+        if parse_comments_structure:
+            try:
+                parent_uuid = ase_structure.comment.strip().split()[-1]
+                aiida_structure_stored.set_extra("parent_uuid", parent_uuid)
+                add_parentstructure_extras(aiida_structure_stored, parent_uuid)
+            except AttributeError:
+                print "could not set parent_uuid on {}".format(ase_structure)
+                pass
+
 
         # add in the chemical formula and number of atoms if possible
         try:
